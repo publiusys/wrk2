e82df59f62580831ba54450971034c274569c5e2
Xiangfeng Zhu <xzhu0027@gmail.com>
2022-09-21 22:41:06 -0700

    wrk fix

--- src/wrk.c
+++ src/wrk.c
@@ -574,11 +574,11 @@ static int response_complete(http_parser *parser) {
     if (cfg.record_all_responses) {
         assert(now > c->actual_latency_start[c->complete & MAXO] );
         uint64_t actual_latency_timing = now - c->actual_latency_start[c->complete & MAXO];
-	if(cfg.print_all_responses) {
-	  printf("%s %"PRIu64" @ %"PRIu64" took %"PRIu64" us\n",
-		 status > 399 ? "failed" : "complete",
-		 c->complete, now, actual_latency_timing);
-	}
+	// if(cfg.print_all_responses) {
+	//   printf("%s %"PRIu64" @ %"PRIu64" took %"PRIu64" us\n",
+	// 	 status > 399 ? "failed" : "complete",
+	// 	 c->complete, now, actual_latency_timing);
+	// }
         hdr_record_value(thread->latency_histogram, actual_latency_timing);
         hdr_record_value(thread->real_latency_histogram, actual_latency_timing);
     
